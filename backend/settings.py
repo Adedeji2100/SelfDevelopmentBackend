978516a164e2dec723d5f32646a65552a36f3822
Adedeji Abisoye <163194719+Adedeji2100@users.noreply.github.com>
2024-07-18 18:08:45 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY = 'django-insecure-0gnb@#zcvnqvz$$9k28uhch5*gr4gt2-#7uh*gtwgo=1*5g6(6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+X_FRAME_OPTIONS = '*'
+CORS_ORIGIN_ALLOW_ALL = True
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/',]
 
 # Application definition
 
